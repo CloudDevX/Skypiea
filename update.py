53b07bf874de53dd4f8c81d0014a86fcdbe1173f
CloudDevX <75575523+CloudDevX@users.noreply.github.com>
2021-05-19 17:45:32 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,7 @@
 #This is the Update file for the Skypiea launcher
-Max = 1
+Max = 3
 if Version != "0.0.1":
     print("le code ici")
+    
+Taches[1] = 'os.mkdir("CHEMIN"+"\.skypiea\launcher")'
+Taches[2] = 'os.mkdir("CHEMIN"+"\.skypiea\mods")'
