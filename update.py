800ebf6d4d14db7fea67c0d098b7e6f49e3935c9
CloudDevX <75575523+CloudDevX@users.noreply.github.com>
2021-05-19 18:11:23 +0100

    Update update.py

--- update.py
+++ update.py
@@ -3,5 +3,5 @@ Max = 2
 if Version != "0.0.1":
     print("le code ici")
     
-Taches[0] = 'os.mkdir("CHEMIN"+r"\.skypiea\launcher")'
-Taches[1] = 'os.mkdir("CHEMIN"+r"\.skypiea\mods")'
+Taches.append(r'os.mkdir("CHEMIN"+r"\.skypiea\launcher")')
+Taches.append(r'os.mkdir("CHEMIN"+r"\.skypiea\mods")')
