61ed5c6c31a638a1093d12da38b5b0876f6ba934
CloudDevX <75575523+CloudDevX@users.noreply.github.com>
2021-05-20 16:33:46 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,10 @@
 #This is the Update file for the Skypiea launcher
 if Version != "0.0.1":   
-    Max = 4
+    Max = 7
     Taches.append(r'os.mkdir(CHEMIN+r"\launcher")')
+    Taches.append(r'Options = open(CHEMIN+r"\launcher\options.txt", "w")')
+    Taches.append(r'Options.write("0.0.1")')
+    Taches.append(r'Options.close()')
     Taches.append(r'os.mkdir(CHEMIN+r"\mods")')
     Taches.append(r'os.mkdir(CHEMIN+r"\assets")')
     Taches.append(r'os.mkdir(CHEMIN+r"\assets\panorama")')
