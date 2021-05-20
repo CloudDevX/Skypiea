6c2b4cc19bd66ae768f190e987265fe7831d7fcc
CloudDevX <75575523+CloudDevX@users.noreply.github.com>
2021-05-20 16:43:22 +0100

    Update update.py

--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 #This is the Update file for the Skypiea launcher
 if Version != "0.0.1":   
-    Max = 7
+    Max = 13
     Taches.append(r'os.mkdir(CHEMIN+r"\launcher")')
     Taches.append(r'Options = open(CHEMIN+r"\launcher\options.txt", "w")')
     Taches.append(r'Options.write("0.0.1")')
@@ -8,6 +8,13 @@ if Version != "0.0.1":
     Taches.append(r'os.mkdir(CHEMIN+r"\mods")')
     Taches.append(r'os.mkdir(CHEMIN+r"\assets")')
     Taches.append(r'os.mkdir(CHEMIN+r"\assets\panorama")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_0.png", CHEMIN+r"\assets\panorama\panorama_0.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_1.png", CHEMIN+r"\assets\panorama\panorama_1.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_2.png", CHEMIN+r"\assets\panorama\panorama_2.png")')
+    #10
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_3.png", CHEMIN+r"\assets\panorama\panorama_3.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_4.png", CHEMIN+r"\assets\panorama\panorama_4.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_5.png", CHEMIN+r"\assets\panorama\panorama_5.png")')
 
 else:
     Loading = Max
