db1bad0b977572c932089e14e19c3b0857c36517
CloudDevX <75575523+CloudDevX@users.noreply.github.com>
2021-05-20 16:57:15 +0100

    Update update.py

--- update.py
+++ update.py
@@ -8,13 +8,13 @@ if Version != "0.0.1":
     Taches.append(r'os.mkdir(CHEMIN+r"\mods")')
     Taches.append(r'os.mkdir(CHEMIN+r"\assets")')
     Taches.append(r'os.mkdir(CHEMIN+r"\assets\panorama")')
-    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_0.png", CHEMIN+r"\assets\panorama\panorama_0.png")')
-    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_1.png", CHEMIN+r"\assets\panorama\panorama_1.png")')
-    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_2.png", CHEMIN+r"\assets\panorama\panorama_2.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_0.png", r"\assets\panorama\panorama_0.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_1.png", r"\assets\panorama\panorama_1.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_2.png", r"\assets\panorama\panorama_2.png")')
     #10
-    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_3.png", CHEMIN+r"\assets\panorama\panorama_3.png")')
-    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_4.png", CHEMIN+r"\assets\panorama\panorama_4.png")')
-    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_5.png", CHEMIN+r"\assets\panorama\panorama_5.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_3.png", r"\assets\panorama\panorama_3.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_4.png", r"\assets\panorama\panorama_4.png")')
+    Taches.append(r'Download("https://raw.githubusercontent.com/CloudDevX/Skypiea/main/Panorama/panorama_5.png", r"\assets\panorama\panorama_5.png")')
 
 else:
     Loading = Max
